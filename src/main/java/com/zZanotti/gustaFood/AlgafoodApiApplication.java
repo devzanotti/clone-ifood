dfa865991d89297208025305b8d10bf9f7bac035
Gustavo Zanotti <guzanottis@gmail.com>
2023-08-04 07:25:13 -0300

    arquivos iniciais

---
+++ src/main/java/com/zZanotti/gustaFood/AlgafoodApiApplication.java
@@ -0,0 +1,14 @@
+package com.zZanotti.gustaFood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgafoodApiApplication {
+
+	public static void main(String[] args) {
+		//SpringApplication.run(AlgafoodApiApplication.class, args);
+		System.out.println("hello");
+	}
+
+}
